2cc68ccac1bf261e7c9a8ddfcfc45725c12ff9e6
torbacz <nanoframework@outlook.com>
2023-01-28 14:07:16 +0100

    Include IServiceCollection on IDevice interface.

--- src/MakoIoT.Device.Services.Interface/IDevice.cs
+++ src/MakoIoT.Device.Services.Interface/IDevice.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using nanoFramework.DependencyInjection;
 
 namespace MakoIoT.Device.Services.Interface
 {
     public interface IDevice
     {
+        public IServiceCollection Services { get; }
+
         void Start();
         void Stop();
 
