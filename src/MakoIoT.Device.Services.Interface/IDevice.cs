3bb96c59af403c9bd1e3ad7acd7d23ab9db57dad
Marcin Torba <torbamarcin@gmail.com>
2023-01-28 15:00:13 +0100

    Move IServiceCollection from IDevice to IDeviceBuilder

--- src/MakoIoT.Device.Services.Interface/IDevice.cs
+++ src/MakoIoT.Device.Services.Interface/IDevice.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using nanoFramework.DependencyInjection;
 
 namespace MakoIoT.Device.Services.Interface
 {
     public interface IDevice
     {
-        public IServiceCollection Services { get; }
-
         void Start();
         void Stop();
 
--- src/MakoIoT.Device.Services.Interface/IDeviceBuilder.cs
+++ src/MakoIoT.Device.Services.Interface/IDeviceBuilder.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using nanoFramework.DependencyInjection;
 
 namespace MakoIoT.Device.Services.Interface
 {
     public delegate void ConfigureDefaultsDelegate(IConfigurationService configurationService);
     public interface IDeviceBuilder
     {
+        public IServiceCollection Services { get; }
+
         ConfigureDefaultsDelegate ConfigureDefaultsAction { get; set; }
         IDeviceBuilder ConfigureDI(Action configureDiAction);
         IDevice Build();
