939ff13d35415ce73742ed6bbf6228652f894f73
Bartek U <2597165+jazzman55@users.noreply.github.com>
2023-05-15 14:02:32 +0200

    Revert "ConfigureDIDelegate"
    This reverts commit 926346c09a9367643bf23f99bb7a0b6cc60f80ff.

--- src/MakoIoT.Device.Services.Interface/IDeviceBuilder.cs
+++ src/MakoIoT.Device.Services.Interface/IDeviceBuilder.cs
@@ -3,7 +3,6 @@ using nanoFramework.DependencyInjection;
 
 namespace MakoIoT.Device.Services.Interface
 {
-    public delegate void ConfigureDIDelegate(IServiceCollection services);
     public delegate void ConfigureDefaultsDelegate(IConfigurationService configurationService);
     public delegate void DeviceStartingDelegate(IDevice device);
     public delegate void DeviceStoppedDelegate(IDevice device);
@@ -13,7 +12,7 @@ namespace MakoIoT.Device.Services.Interface
         public IServiceCollection Services { get; }
 
         ConfigureDefaultsDelegate ConfigureDefaultsAction { get; set; }
-        IDeviceBuilder ConfigureDI(ConfigureDIDelegate configureDiAction);
+        IDeviceBuilder ConfigureDI(Action configureDiAction);
         IDevice Build();
         event DeviceStartingDelegate DeviceStarting;
         event DeviceStoppedDelegate DeviceStopped;
