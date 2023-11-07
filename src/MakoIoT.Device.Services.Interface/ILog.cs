a4c88973a1257194e32eef2d15040a7c49893652
Marcin Torba <torbamarcin@gmail.com>
2023-11-07 23:51:20 +0100

    Create interfaces for new logging system.

---
+++ src/MakoIoT.Device.Services.Interface/ILog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+
+namespace MakoIoT.Device.Services.Interface
+{
+    public interface ILog
+    {
+        void Trace(string message, Exception exception, MethodInfo format);
+        void Trace(string message);
+        void Trace(Exception exception);
+
+        void Information(string message, Exception exception, MethodInfo format);
+        void Information(string message);
+        void Information(Exception exception);
+
+        void Warning(string message, Exception exception, MethodInfo format);
+        void Warning(string message);
+        void Warning(Exception exception);
+
+        void Error(string message, Exception exception, MethodInfo format);
+        void Error(string message);
+        void Error(Exception exception);
+
+        void Critical(string message, Exception exception, MethodInfo format);
+        void Critical(string message);
+        void Critical(Exception exception);
+    }
+}
